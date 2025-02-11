c7dcccea250915ca62cb349e0d9dc17fb25b9c75
meiyunchao <2511633874@qq.com>
2025-02-11 11:23:56 +0800

    'deploy'

--- .vitepress/theme/index.mjs
+++ .vitepress/theme/index.mjs
@@ -1,13 +1,12 @@
 import DefaultTheme from "vitepress/theme";
 
-import "simple-mind-map/dist/simpleMindMap.esm.min.css";
-
 export default {
 	...DefaultTheme,
 	async enhanceApp({ app }) {
 		if (!import.meta.env.SSR) {
 			// await import("vitepress-vue3-components/dist/style.css");
 			// const plugin = await import("vitepress-vue3-components");
+			await import("simple-mind-map/dist/simpleMindMap.esm.min.css");
 			const Mindmap = await import("./components/Mindmap.vue");
 			app.component("ElMindmap", Mindmap.default);
 		}
